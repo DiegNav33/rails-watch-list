49a1df41788a16c465c8cf286dce4f2f648b2b23
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-08-15 16:43:43 +0800

    Few edits on the views

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,20 @@ class List < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_one_attached :image
   validates :name, presence: true, uniqueness: true
+  validate :image_type
+  validate :image_size
+
+  private
+
+  def image_type
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/png image/jpg))
+      errors.add(:image, 'Must be a JPEG or PNG')
+    end
+  end
+
+  def image_size
+    if image.attached? && image.byte_size > 5.megabytes
+      errors.add(:image, 'Size should be less than 5MB')
+    end
+  end
 end
